e8c1a7545a10cbe8267567e561cb5903f991e2a7
Travis CI User <travis@example.org>
2017-11-25 17:39:21 +0000

    Documentation for alexcrichton/filetime@3ebf47a

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["filetime"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"filetime/struct.FileTime.html\" title=\"struct filetime::FileTime\">FileTime</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
